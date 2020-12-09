064b94f8eec36511e73c24b9b5a7fe75762d08fa
tudormihaiuc <61910489+tudormihaiuc@users.noreply.github.com>
2020-12-09 10:29:09 +0200

    added sentences and phone number recognition

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import nltk
+#nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
 
 def function():
-    nrOfLetters=0;
+    nrOfLetters=0
     frOfLetters = {}
     percentages = {}
+    phoneNumbers=[]
+    cnp={}
     file = open("test.txt", "rt")
     data=file.read()
+    nrOfSentences=sent_tokenize(data)
     for char in data:
         if char.isalpha():
             char=char.upper()
@@ -18,13 +24,22 @@ def function():
     for char in frOfLetters:
         if char.isalpha():
            percentages[char]=frOfLetters[char]/nrOfLetters*100
-    for i in words:
-        if i.isnumeric():
-            words.remove(i)
-    print(words)
+        x = len(words)
+        i = 0
+        while (i < x):
+            if words[i].isnumeric():
+                if words[i][0] == "0" and words[i][1] == "7" and len(words[i]) == 10:
+                        phoneNumbers.append(words[i])
+                words.remove(words[i])
+                x = len(words)
+            else:
+                i += 1
     print("Cuvinte = ",len(words))
+    print("Propozitii: ",len(nrOfSentences))
     print("Litere: ")
     for i in frOfLetters:
         print(i," = ",frOfLetters[i]," (",percentages[i],"%)")
+    resList = list(dict.fromkeys(phoneNumbers))
+    print("Telefoane= ", len(resList),resList)
 
 function()
